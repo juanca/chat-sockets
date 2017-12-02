175405eced9a739da1d6cbc05bf6bceabdeb5731
Juan Carlos Medina <juanca@mavenlink.com>
2017-12-01 23:09:56 -0800

    wip

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+import { createStore } from 'redux';
+
 console.log('hello world');
+
+const initialState = [];
+
+const store = createStore((state = initialState, action) => {
+  switch(action.type) {
+    case 'NEW_MESSAGE': return state.concat(action.payload);
+    default: return state;
+  }
+});
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+store.dispatch({ type: 'NEW_MESSAGE', payload: 'poops' });
 
 const websocket = new WebSocket('ws://localhost:8080');
 
