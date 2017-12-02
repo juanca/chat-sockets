7af5f432c78e2d0fba5811b1fdc7335ed24f29e6
Juan Carlos Medina <juanca@mavenlink.com>
2017-12-01 21:18:14 -0800

    websocket server init

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('hello world');
+
+const websocket = new WebSocket('ws://localhost:8080');
+
+websocket.onopen = function (event) {
+  websocket.send("Here's some text that the server is urgently awaiting!");
+};
